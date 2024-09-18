7f7da4d9e81e534abe1ec5c52e937929c83102c9
fazlehaq <fazlehaqs360@gmail.com>
2024-09-18 13:30:21 +0530

    optimized the loop to cover maximum pointers in one file read

--- ls.c
+++ ls.c
@@ -7,6 +7,9 @@
 #include"disk_utils.h"
 #include"math_utils.h"
 
+// Calculating the max number of bytes to store the encoding of the pointer
+#define MAX_BYTES_PER_FILE_POINTER_ENCODING myCeilDiv ((25*8)+getNumOfBitsToEncode(25)+(2*(getNumOfBitsToEncode(0xFFFFFFFFFFFFFFFF))),8)
+
 
 void ls (FILE *vdfp){
     ull fileCnt = disk_state.fileCount;
@@ -14,14 +17,16 @@ void ls (FILE *vdfp){
         printf("\n********No files in the VD********\n");
         return; 
     }
-
+    
+    int maxPointersInBuffer = (MAX_BUFFER_SIZE/MAX_BYTES_PER_FILE_POINTER_ENCODING);// calculating how many complete pointers we can have in the buffer 
+    int currPointerCnt = 0; // keeping track for the pointer read from buffer
     unsigned char encoding[MAX_BUFFER_SIZE];
     ull byteCnt = 0;
     fseek(vdfp,32,SEEK_SET);
     fread(encoding,MAX_BUFFER_SIZE,1,vdfp);
+    int bitCnt = 0;
 
     while(fileCnt != 0){
-        int bitCnt = 0;
         int fileNameLength = 0;
         char fileName[MAX_FILE_NAME_LENGTH] = {0};
         ull fileSize = 0;
@@ -42,11 +47,15 @@ void ls (FILE *vdfp){
         printf("%s\t%llu\t%llu\n",fileName,fileSize,filePointer);
         
         fileCnt--;
+        currPointerCnt++;
 
         if(fileCnt == 0) break;
         bitCnt = myCeilDiv(bitCnt,8) *8;
         byteCnt += myCeilDiv(bitCnt,8);
-        fseek(vdfp, byteCnt + 32,SEEK_SET);
-        fread(encoding,1,MAX_BUFFER_SIZE,vdfp);
+        if(currPointerCnt == maxPointersInBuffer){
+            fseek(vdfp, byteCnt + 32,SEEK_SET);
+            fread(encoding,1,MAX_BUFFER_SIZE,vdfp);
+            currPointerCnt = 0;
+        }
     }
 }
