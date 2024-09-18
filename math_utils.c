94d76e5f02d5b05c4cd2b964665a59aaca0f291b
fazlehaq <fazlehaqs360@gmail.com>
2024-09-18 13:29:20 +0530

    updated and corrected ceil division function

--- math_utils.c
+++ math_utils.c
@@ -13,5 +13,6 @@ ull myPow(ull num, ull x){
 
 // Custom Ceil Division
 ull myCeilDiv(ull x,ull y){
-	return (unsigned int) ceil(((double) x)/((double) y));
+	// return (unsigned int) ceil(((double) x)/((double) y));
+    return x/y + ((x%y == 0) ? 0 : 1);
 }
