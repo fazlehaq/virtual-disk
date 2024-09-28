0712d4f4dcb9d486c2ab9d47622cabaad440041d
fazlehaq <fazlehaqs360@gmail.com>
2024-09-28 23:06:08 +0530

    updated get file to work with removed file bit and utilise the memory

--- src/get_file.c
+++ src/get_file.c
@@ -12,50 +12,71 @@
 void getFile(FILE *vdfp){
     if(disk_state.fileCount == 0) { printf("No files in the vd\n"); return; }
     char fileName[MAX_FILE_NAME_LENGTH] = {0};
+    printf("Enter the name of the file : ");
     scanf("%s",fileName);
 
     if(strlen(fileName) == 0) { printf("Please enter valid filename\n"); return; }
 
     ull fileCount = disk_state.fileCount;
-    unsigned char buffer[MAX_BUFFER_SIZE];
+    int maxPointersInBuffer = (MAX_BUFFER_SIZE/MAX_BYTES_PER_FILE_POINTER_ENCODING);// calculating how many complete pointers we can have in the buffer
+    int currPointerCnt = 0;
+    unsigned char encoding[MAX_BUFFER_SIZE];
+    ull byteCnt = 0;
+    int encodingBitCnt = 0;
     fseek(vdfp,32,SEEK_SET);
-    fread(buffer,MAX_BUFFER_SIZE,1,vdfp);
-    int byteCnt = 0;
+    fread(encoding,MAX_BUFFER_SIZE,1,vdfp);
 
     // infinite loop
     while (1){
-        int bitCnt = 0;
+        int currFileNameLength = 0;
         char currFileName[MAX_FILE_NAME_LENGTH] = {0};
         ull fileSize = 0;
         ull filePointer = 0;
+        int bitReadCnt = 0;
+        int isRemoved = 0;
 
-        int currFileNameLength = decode(buffer,bitCnt);
-        bitCnt += getNumOfBitsToEncode(currFileNameLength);
+        isRemoved = getbit(encoding,encodingBitCnt+bitReadCnt);
+        bitReadCnt++;
+
+        currFileNameLength = decode(encoding,encodingBitCnt+bitReadCnt);
+        bitReadCnt += getNumOfBitsToEncode(currFileNameLength);
+
         for(int i=0;i<8*currFileNameLength;i++)
-            setbit((unsigned char *)currFileName,i,getbit(buffer,i+bitCnt));
-        bitCnt += (8*currFileNameLength);
+            setbit((unsigned char *)currFileName,i,getbit(encoding,i+ encodingBitCnt+bitReadCnt));
+        bitReadCnt += (8*currFileNameLength);
 
-        fileSize = decode(buffer,bitCnt);
-        bitCnt += getNumOfBitsToEncode(fileSize);
+        fileSize = decode(encoding,encodingBitCnt+bitReadCnt);
+        bitReadCnt += getNumOfBitsToEncode(fileSize);
 
-        filePointer = decode(buffer,bitCnt);
-        bitCnt += getNumOfBitsToEncode(filePointer);
+        filePointer = decode(encoding,encodingBitCnt+bitReadCnt);
+        bitReadCnt += getNumOfBitsToEncode(filePointer);
 
-        if(strcmp(fileName,currFileName) == 0) { 
+        if(isRemoved == 0 && strcmp(fileName,currFileName) == 0) { 
             // vdfile *matchedFile = (vdfile *) malloc(sizeof(vdfile));
             printf("File found\n"); 
             fseek(vdfp,filePointer,SEEK_SET);
             FILE * output= fopen("output","wb");
             copyfile(vdfp,output,fileSize);
             fclose(output);
+            printf("File copied in output\n"); 
             return; 
         }
-        fileCount--;
+        
+        if(isRemoved == 0)
+            fileCount--;
+
         if(fileCount == 0) {printf("File not found \n"); break;}
 
-        byteCnt += myCeilDiv(bitCnt,8);
-        fseek(vdfp,32+byteCnt,SEEK_SET);
-        fread(buffer,MAX_BUFFER_SIZE,1,vdfp);
+        encodingBitCnt += myCeilDiv(bitReadCnt,8) * 8;
+        byteCnt += myCeilDiv(bitReadCnt,8);
+
+        if(currPointerCnt == maxPointersInBuffer){
+
+            fseek(vdfp,32+byteCnt,SEEK_SET);
+            fread(encoding,MAX_BUFFER_SIZE,1,vdfp);
+            currPointerCnt = 0;
+            encodingBitCnt = 0;
+        }
     }
     
 }
